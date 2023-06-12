fef81b6ae3431ca43ce13baaa496fe73111fdbe5
L <cheungwailokhk@gmail.com>
2023-06-12 14:21:45 +0100

    [php_code_challenge-main] Reader Class
    - Add reader class to read csv file
    - Add Singleton class

---
+++ src/utils/Reader.php
@@ -0,0 +1,26 @@
+<?php
+
+require 'src/utils/Singleton.php'; 
+
+class Reader extends Singleton
+{
+    /**
+     * Read CSV file 
+     * @param string      $path     Path of CSV file
+     * @return array    array of array
+     */
+    function readCSV($path)
+    {
+        $file = fopen($path, "r");
+        $arr = [];
+        while (!feof($file)) {
+            array_push($arr, fgetcsv($file));
+        }
+
+        fclose($file);
+
+        return $arr;
+    }
+}
+
+?>
---
+++ src/utils/Singleton.php
@@ -0,0 +1,16 @@
+<?php
+class Singleton
+{
+    private static $instances = [];
+
+    public static function getInstance()
+    {
+        $subclass = static::class;
+        if (!isset(self::$instances[$subclass])) {
+            self::$instances[$subclass] = new static();
+        }
+        return self::$instances[$subclass];
+    }
+}
+
+?>
